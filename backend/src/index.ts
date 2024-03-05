95d74a012338df51a07b6cdce87da842bce3dccc
diego <dcorreanavarrete@gmail.com>
2024-03-05 14:41:29 -0600

    agregando la variable de env en el servidor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server update");
 });
 
-app.listen(3001, () => {
-  console.log(`[server]: Server is running at http://localhost:${3001}`);
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
