ae4579d4ed9aa537451d7c2bc4334296a8b7ad12
diego <dcorreanavarrete@gmail.com>
2024-03-05 22:46:53 -0600

    eliminando cosas innecesarias de la config del server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
-import express, { Express } from "express";
 import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
-
-const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
