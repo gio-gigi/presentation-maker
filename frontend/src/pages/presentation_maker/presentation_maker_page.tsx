06ad8945db1f6f36abbfadc51d5af8dadd3936b4
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-06 17:10:00 -0600

    Incorporacion de la hoja de estilos a la pagina de creacion de presentaciones

--- frontend/src/pages/presentation_maker/presentation_maker_page.tsx
+++ frontend/src/pages/presentation_maker/presentation_maker_page.tsx
@@ -1,8 +1,10 @@
 import { PresentationEditor } from '../../components/presentation_editor/presentation_editor';
 import { PresentationPreview } from '../../components/presentation_preview/presentation_preview';
+import './presentation_maker_page.css';
+
 export const PresentationMakerPage = () => {
     return (
-        <div>
+        <div className='pm-page'>
             <PresentationEditor />
             <PresentationPreview />
         </div>
