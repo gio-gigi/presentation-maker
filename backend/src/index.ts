41d8dda6740796a2cff6eb957ae09bd5b9a0d0c4
diego <dcorreanavarrete@gmail.com>
2024-03-06 17:56:16 -0600

    agregando el datasource al servidor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 import app from "./app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "./config/data-source";
 
 dotenv.config();
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
+
+export default app;
