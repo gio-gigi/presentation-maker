ef3f4f439164d604ddccfd3f0d6f05db0081d996
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-06 07:50:09 -0600

    Creacion de la seccion de preview de la presentacion

---
+++ frontend/src/components/presentation_preview/presentation_preview.tsx
@@ -0,0 +1,10 @@
+export const PresentationPreview = () => {
+    return (
+        <div>
+            <h2>Preview de la presentación</h2>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
