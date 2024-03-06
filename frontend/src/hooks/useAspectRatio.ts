287c6f133849ed6730bdab1d07f77980ddc4fc3b
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-06 17:10:29 -0600

    Creacion de un custom hook que maneja el aspect ratio de un componente HTML

---
+++ frontend/src/hooks/useAspectRatio.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from "react"
+import { JsxElement } from "typescript";
+
+export enum BasedOn {
+    HORIZONTAL,
+    VERTICAL
+}
+
+interface useAspectRatioProps {
+    basedOn: BasedOn;
+    ratio: number;
+}
+
+export const useAspectRatio = ({basedOn, ratio}: useAspectRatioProps) => {
+    const elementReference = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        const resize = () => {
+            if (elementReference.current) {
+                if (basedOn === BasedOn.HORIZONTAL) {
+                    elementReference.current.style.minHeight = `${elementReference.current.offsetWidth * ratio}px`;
+                } else {
+                    elementReference.current.style.minWidth = `${elementReference.current.offsetHeight * ratio}px`;
+                }
+            }
+        }
+        window.addEventListener('resize', resize);
+        resize();
+        return () => {
+            window.removeEventListener('resize', resize);
+        }
+    }, []);
+    return {elementReference};
+}
