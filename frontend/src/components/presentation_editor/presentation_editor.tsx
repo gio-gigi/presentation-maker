f6a4081a27fb0e9fc6fc5e5343c6d9592bff506b
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-06 17:06:27 -0600

    Adicion de los estilos al componenete Editor de presentacion

--- frontend/src/components/presentation_editor/presentation_editor.tsx
+++ frontend/src/components/presentation_editor/presentation_editor.tsx
@@ -1,8 +1,10 @@
+import './presentation_editor.css';
+
 export const PresentationEditor = () => {
     return (
-        <div>
-            <h2>Ingresa el contenido de la presentación</h2>
-            <textarea></textarea>
+        <div className='pe-container'>
+            <h2 className='title'>Ingresa el contenido de la presentación</h2>
+            <textarea className='text-container'></textarea>
         </div>
     );
 }
