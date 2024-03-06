158363d6acb006b64ec5220426fc956606af3828
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-05 18:55:25 -0600

    Ampliacion del conjunto de prueba para desarrollo

--- frontend/src/data/datasources/dev/presentation_datasource.ts
+++ frontend/src/data/datasources/dev/presentation_datasource.ts
@@ -9,7 +9,7 @@ export class PresentationDatasourceDev implements PresentationDatasource {
 const presentationList: Presentation[] = [
     {
         id: '1',
-        title: 'Presentation 1',
+        title: 'Lorem Ipsum Dolor Sit Amet Consectetur Adipiscing Elit Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua',
         description: 'Description 1',
         author: 'Author 1',
         createdAt: new Date('2021-01-01T00:00:00Z')
@@ -23,6 +23,27 @@ const presentationList: Presentation[] = [
     },
     {
         id: '3',
+        title: 'Presentation 3',
+        description: 'Description 3',
+        author: 'Author 3',
+        createdAt: new Date('2021-03-01T00:00:00Z')
+    },
+    {
+        id: '4',
+        title: 'Presentation 1',
+        description: 'Description 1',
+        author: 'Author 1',
+        createdAt: new Date('2021-01-01T00:00:00Z')
+    },
+    {
+        id: '5',
+        title: 'Presentation 2',
+        description: 'Description 2',
+        author: 'Author 2',
+        createdAt: new Date('2021-02-01T00:00:00Z')
+    },
+    {
+        id: '6',
         title: 'Presentation 3',
         description: 'Description 3',
         author: 'Author 3',
