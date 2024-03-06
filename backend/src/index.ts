5a3bebeddf8c28116a8b2cb4fc0d2d378cf6024e
diego <dcorreanavarrete@gmail.com>
2024-03-05 22:35:18 -0600

    removiendo ruta de prueba

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server update");
-});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
