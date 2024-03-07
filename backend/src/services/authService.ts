378c31b165cb22e8390ff171897fadaf43068f33
diego <dcorreanavarrete@gmail.com>
2024-03-07 07:35:09 -0600

    agregando metodo para crear usuario

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -5,8 +5,10 @@ import UnauthorizedError from "../errors/UnauthorizedError";
 import jwt from "jsonwebtoken";
 import { ILoginResult } from "../interfaces/ILoginResult";
 import dotenv from "dotenv";
+import { userExists } from "./userService";
 const UserRepository = AppDataSource.getRepository(User);
 dotenv.config();
+const SECRET_JWT: string | undefined = process.env.SECRET_JWT;
 
 export class AuthService {
   async loginUser(email: string, pwd: string): Promise<ILoginResult> {
@@ -18,13 +20,34 @@ export class AuthService {
     if (!passwordMatch) {
       throw new UnauthorizedError({ message: "Passwords dont match", logging: true });
     }
-    const SECRET_JWT: string | undefined = process.env.SECRET_JWT;
-    const token: string = await jwt.sign(email, SECRET_JWT || "mysecret");
+    const token: string = await this.getJwt(email);
     return { isAdmin: user.admin, email, token };
+  }
+
+  async createUser(email: string, pwd: string, name: string, admin: boolean): Promise<User> {
+    const user: User = new User();
+    user.email = email;
+    user.password = await this.hashPwd(pwd);
+    user.name = name;
+    user.admin = admin;
+    if (await userExists(email)) throw new UnauthorizedError({ message: "User already exists", logging: true });
+    await UserRepository.save(user);
+    return user;
+  }
+
+  async getJwt(email: string): Promise<string> {
+    const token: string = await jwt.sign(email, SECRET_JWT || "mysecret ");
+    return token;
   }
 
   private async comparePwd(dbPwd: string, userPwd: string): Promise<boolean> {
     const ans: boolean = await bcrypt.compare(userPwd, dbPwd);
     return ans;
+  }
+
+  private async hashPwd(pwd: string): Promise<string> {
+    const salt: string = await bcrypt.genSalt(10);
+    const hashedPwd: string = await bcrypt.hash(pwd, salt);
+    return hashedPwd;
   }
 }
