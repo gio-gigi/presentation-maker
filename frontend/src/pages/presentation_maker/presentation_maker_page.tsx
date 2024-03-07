ce42856cb403ae79cb626145f8b790b3242b873a
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-07 11:46:42 -0600

    Se agrego el hook use presentation maker a la pagina del mismo nombre para compartir los datos entre el editor y la vista previa

--- frontend/src/pages/presentation_maker/presentation_maker_page.tsx
+++ frontend/src/pages/presentation_maker/presentation_maker_page.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import { PresentationEditor } from '../../components/presentation_editor/presentation_editor';
 import { PresentationPreview } from '../../components/presentation_preview/presentation_preview';
 import './presentation_maker_page.css';
+import { usePresentationMaker } from '../../hooks/usePresentationMaker';
 
 export const PresentationMakerPage = () => {
+    const {slides, onTextChange} = usePresentationMaker();
     return (
         <div className='pm-page'>
-            <PresentationEditor />
-            <PresentationPreview />
+            <PresentationEditor onTextChange={onTextChange}/>
+            <PresentationPreview slides={slides}/>
         </div>
     );
 };
