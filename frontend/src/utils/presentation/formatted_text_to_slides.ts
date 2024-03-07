52428ecc2f371a1517721f3c6b0e6f46a8e4e9ce
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-06 20:53:51 -0600

    Creacion de la clase que se encarga de convertir el texto formateado a slides

--- frontend/src/utils/presentation/formatted_text_to_slides.ts
+++ frontend/src/utils/presentation/formatted_text_to_slides.ts
@@ -1,15 +1,46 @@
+import { AvailableFontWeights, AvailableFonts } from '../../constants/font';
+import { SlideTextEntity, VisualizableSlideEntity } from '../../infrastructure/entities/presentation_preview_entity';
 import { PresentationMakerPage } from '../../pages/presentation_maker/presentation_maker_page';
-const extractSlides = (formattedText: string): string[] => {
-    const pattern = /\\slide\{(\s)*([\s\S]*?)(\s)*\}/g;
-    const result = formattedText.matchAll(pattern);
-    let slides: string[] = [];
-    for (const match of result) {
-        slides.push(match[2]);
+export class Slides {
+    private static extractSlides(formattedText: string): string[] {
+        const pattern = /\\slide\{(\s)*([\s\S]*?)(\s)*\}/g;
+        const result = formattedText.matchAll(pattern);
+        let slides: string[] = [];
+        for (const match of result) {
+            slides.push(match[2]);
+        }
+        return slides;
     }
-    return slides;
-}
 
+    private static extractSlideContent(slide: string): SlideTextEntity[] {
+        const pattern = /\\text\[(\d+),([a-zA-Z]+),([a-z]+)\]\{(\s)*([\s\S]*?)(\s)*\}/g;
+        const result = slide.matchAll(pattern);
+        let content: SlideTextEntity[] = [];
+        for (const match of result) {
+            const isValidFontName = AvailableFonts.some((font) => font === match[2]);
+            const isValidFontWeight = AvailableFontWeights.some((font) => font === match[3]);
+            if (!isValidFontName || !isValidFontWeight) {
+                continue;
+            }
+            const lines = match[5].split('\n');
+            content.push({
+                lines: lines,
+                fontSize: parseInt(match[1]),
+                fontFamily: match[2],
+                fontWeigth: match[3]
+            });
+        }
+        return content;
+    }
 
-export const formattedTextToSlides = (formattedText: string): string[] => {
-    return extractSlides(formattedText);
+    public static formattedTextToSlides(formattedText: string): VisualizableSlideEntity[] {
+        const slidesWithoutFormat = this.extractSlides(formattedText);
+        let slides: VisualizableSlideEntity[] = [];
+        slidesWithoutFormat.forEach((slide) => {
+            slides.push({
+                content: this.extractSlideContent(slide)
+            });
+        });
+        return slides;
+    }
 }
