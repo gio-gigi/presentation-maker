79101155899d5a1709b58e176ebf17c690766c2f
diego <dcorreanavarrete@gmail.com>
2024-03-07 18:33:51 -0600

    agregando metodo que devuelve el rol del usuario

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,10 +1,17 @@
-import { User } from "../models/User";
+import { User, UserRole } from "../models/User";
 import { AppDataSource } from "../config/data-source";
-import { IUser } from "../interfaces/IUser";
 
 const UserRepository = AppDataSource.getRepository(User);
 
 export const userExists = async (email: string): Promise<boolean> => {
-  const user: IUser | null = await UserRepository.findOneBy({ email });
+  const user: User | null = await UserRepository.findOneBy({ email });
   return user ? true : false;
+};
+
+export const getUserRole = async (email: string): Promise<UserRole | null> => {
+  const user: User | null = await UserRepository.findOneBy({ email });
+  if (user) {
+    return user.role;
+  }
+  return null;
 };
