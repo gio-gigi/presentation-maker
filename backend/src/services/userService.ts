ed45e0f466202ade6a9afb9078c2d845f810e7b9
diego <dcorreanavarrete@gmail.com>
2024-03-07 18:39:34 -0600

    cambaindo ruta anterior

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -2,16 +2,16 @@ import { User, UserRole } from "../models/User";
 import { AppDataSource } from "../config/data-source";
 
 const UserRepository = AppDataSource.getRepository(User);
-
-export const userExists = async (email: string): Promise<boolean> => {
-  const user: User | null = await UserRepository.findOneBy({ email });
-  return user ? true : false;
-};
-
-export const getUserRole = async (email: string): Promise<UserRole | null> => {
-  const user: User | null = await UserRepository.findOneBy({ email });
-  if (user) {
-    return user.role;
+export class UserService {
+  static async userExists(email: string): Promise<boolean> {
+    const user: User | null = await UserRepository.findOneBy({ email });
+    return user ? true : false;
   }
-  return null;
-};
+  static async getUserRole(email: string): Promise<UserRole | null> {
+    const user: User | null = await UserRepository.findOneBy({ email });
+    if (user) {
+      return user.role;
+    }
+    return null;
+  }
+}
