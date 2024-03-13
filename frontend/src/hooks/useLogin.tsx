00e3b6993c43965a60d9be274cd761a804958801
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-03-13 14:42:59 -0600

    Se consigue el token de la api y lo muestra en la consola

--- frontend/src/hooks/useLogin.tsx
+++ frontend/src/hooks/useLogin.tsx
@@ -2,28 +2,31 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { FormValues } from "../infrastructure/entities/login_form_values";
 import { LoginDatasourceDev } from "../data/datasources/dev/login_datasource";
 import { LoginRepositoryDev } from "../data/repositories/login_repository";
-import BadRequestError from "../errors/BadRequestError";
-import UnauthorizedError from "../errors/UnautherizedError";
 const loginDatasource = new LoginDatasourceDev();
 const loginRepository = new LoginRepositoryDev(loginDatasource)
 
 export const useLogin = () => {
-    const {register, handleSubmit} = useForm<FormValues>()
+    const {
+        register, 
+        handleSubmit,
+        setError,
+        formState : {errors}
+    } = useForm<FormValues>()
 
-    const onSubmit: SubmitHandler<FormValues> = (data) => {
+    const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => {
         const authenticate = async () => {
-            const {email, password} = data
             try{
-                //const token = await loginRepository.getToken(data);
-                if(password==='admin'){
-                    alert("Bienvenido")
-                }
-            }catch(UnautherizedError){
-                alert("Datos incorrectos")
+                const token = await loginRepository.getToken(data);
+                console.log(token)
+            }catch(error: any){
+                setError("email",{
+                    type:"manual", 
+                    message:error.response.data.errors[0].message,
+                })
             }
         }
         authenticate();
     }
 
-    return {register, handleSubmit, onSubmit};
+    return {register, handleSubmit, onSubmit, errors};
 }
