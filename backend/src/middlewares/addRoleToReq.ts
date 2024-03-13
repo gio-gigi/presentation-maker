20da3f42420ca80f2e01044575e8490cc43991a8
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-03-13 12:42:39 -0600

    renombrando archivo

--- backend/src/middlewares/addRoleToReq.ts
+++
@@ -1,7 +0,0 @@
-// import jwt from "jsonwebtoken";
-// import { Request, Response, NextFunction } from "express";
-
-// export const addRoleToReq = (req: Request, res: Response, next: NextFunction): void => {
-//   req.admin = false;
-//   next();
-// };
