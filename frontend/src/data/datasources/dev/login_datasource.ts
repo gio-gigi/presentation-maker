c882d6560658083a5ae03735eb9d066808f5b057
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-03-13 14:42:47 -0600

    Se corrigio el url de la api

--- frontend/src/data/datasources/dev/login_datasource.ts
+++ frontend/src/data/datasources/dev/login_datasource.ts
@@ -1,25 +1,17 @@
-import BadRequestError from "../../../errors/BadRequestError";
-import UnauthorizedError from "../../../errors/UnautherizedError";
 import { LoginDatasource } from "../../../infrastructure/datasources/login_datasource";
 import { FormValues } from "../../../infrastructure/entities/login_form_values";
 import axios from "axios";
 
-const API_URL = "https://localhost:3001/api/auth/signin"
+const API_URL = "http://localhost:3001"
 
 export class LoginDatasourceDev implements LoginDatasource{
     async getToken(form: FormValues): Promise<string> {
         const {email, password} = form
-        try {
-            const response = await axios.post<{token: string}>(API_URL, {email, password});
-            if (response.status === 200) {
-                return response.data.token;
-            } else if (response.status === 401) {
-                throw new UnauthorizedError()
-            } else {
-                throw new BadRequestError()
-            }
-        } catch (error) {
-            throw error;
-        }
+        const response = await axios.post<{token: string}>(API_URL+'/api/auth/login',
+            {
+                "email": email,
+                "password": password
+            });
+        return response.data.token;
     }
 }   
