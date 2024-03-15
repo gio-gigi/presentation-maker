9f2c38370a883176b79a025a05f33ca20aec5e96
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-15 14:03:57 -0600

    Se cambio el aspect ratio a un parametro

--- frontend/src/components/presentation_preview/presentation_preview.tsx
+++ frontend/src/components/presentation_preview/presentation_preview.tsx
@@ -1,21 +1,30 @@
-
-import { VisualizableSlideEntity } from '../../infrastructure/entities/presentation_preview_entity';
-import { Slide } from '../slide/slide';
-import './presentation_preview.css';
+import { AspectRatioEnum } from "../../constants/aspect_ratio";
+import { BasedOn } from "../../hooks/useAspectRatio";
+import { VisualizableSlideEntity } from "../../infrastructure/entities/presentation_preview_entity";
+import { Slide } from "../slide/slide";
+import "./presentation_preview.css";
 interface PresentationPreviewProps {
-    slides: VisualizableSlideEntity[];
+  slides: VisualizableSlideEntity[];
 }
-export const PresentationPreview = ({slides}: PresentationPreviewProps) => {
-    return (
-        <div className="ppw-container">
-            <h2 className="title">Preview de la presentación</h2>
-            <div className='slides-list'>
-                {
-                    slides.map((slide, index) => {
-                        return <Slide key={`slide-${index}`} id = {`slide-${index}`} slide={slide}/>
-                    })
-                }
-            </div>
-        </div>
-    );
-}
+export const PresentationPreview = ({ slides }: PresentationPreviewProps) => {
+  return (
+    <div className="ppw-container">
+      <h2 className="title">Preview de la presentación</h2>
+      <div className="slides-list">
+        {slides.map((slide, index) => {
+          return (
+            <Slide
+              key={`slide-${index}`}
+              id={`slide-${index}`}
+              slide={slide}
+              aspectRatio={{
+                basedOn: BasedOn.WIDTH,
+                ratio: AspectRatioEnum.NINE_SIXTEEN,
+              }}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
