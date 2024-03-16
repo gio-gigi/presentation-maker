2f1e94c8b884b6ea96d7ba84ab92917a82d911d8
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-03-15 22:39:20 -0600

    agregando la base de la url de las imagenes

--- backend/src/constants/paths.ts
+++ backend/src/constants/paths.ts
@@ -1,2 +1,5 @@
+import { SERVER_PORT } from "./server";
+
 export const PATH_TO_STATICS_TXT = "static/presentations/txt/";
 export const PATH_TO_STATICS_IMG = "static/presentations/img/";
+export const BASE_IMG_URL = `http://localhost:${SERVER_PORT}/img/`;
