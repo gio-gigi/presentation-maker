577021163b44cdfb13ccead3d0cfdf9ddedd0de1
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-17 11:59:38 -0600

    Se implemento el click en una presentacion para que esta pase a modo presentacion

--- frontend/src/components/presentation_tile/presentation_tile.tsx
+++ frontend/src/components/presentation_tile/presentation_tile.tsx
@@ -1,14 +1,19 @@
 import { PresentationInfoEntity } from '../../infrastructure/entities/presentation_entity';
 import placeholderImage from '../../assets/image_placeholder.jpg';
 import './presentation_tile.css';
+import { useNavigate } from 'react-router-dom';
 
 interface PresentationTileProps {
     presentation: PresentationInfoEntity;
 }
 
 export const PresentationTile = ({presentation}: PresentationTileProps) => {
+    const navigate = useNavigate();
+    const handleClicked = () => {
+        navigate(`/presentation/${presentation.id}`);
+    }
     return (
-        <div key={presentation.id} className='tile'>
+        <div key={presentation.id} className='tile' onClick={handleClicked}>
             <div className="sub-tile">
                 <div className="image-container">
                     <img className ='placeholder_img' src={presentation.imageUrl ?? placeholderImage}/>
