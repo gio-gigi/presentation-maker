1066ac562af21a2e0790ca8f75194a8417be1cc6
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-17 11:58:41 -0600

    Se agrego el contexto de autorizacion a la app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
+import { AuthContextProvider } from './contexts/auth_context';
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router}/>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
     </div>
   );
 }
