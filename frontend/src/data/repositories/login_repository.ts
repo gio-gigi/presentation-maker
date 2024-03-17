72fe805eba50cc2c056871f1573e4f3794ff64bb
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-17 11:57:47 -0600

    Se cambio la logica de obtencion de informacion en el inicio de sesion

--- frontend/src/data/repositories/login_repository.ts
+++ frontend/src/data/repositories/login_repository.ts
@@ -1,12 +1,13 @@
 import { LoginDatasource } from "../../infrastructure/datasources/login_datasource";
 import { FormValues } from "../../infrastructure/entities/login_form_values";
+import { LoggedInUser } from "../../infrastructure/entities/user";
 import { LoginRepository } from "../../infrastructure/repositories/login_repository";
 
 export class LoginRepositoryDev implements LoginRepository{
     constructor(
         private datasource: LoginDatasource
     ){}
-    async getToken(form: FormValues): Promise<string> {
+    async getToken(form: FormValues): Promise<LoggedInUser> {
         return this.datasource.getToken(form);
     }
 }
