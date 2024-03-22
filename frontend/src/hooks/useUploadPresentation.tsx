cdf36e720cf68e6c2ff9d62460cd42fd8daf3cb1
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-21 20:36:13 -0600

    Se actualizó la logica para agregar el titulo en la peticion

--- frontend/src/hooks/useUploadPresentation.tsx
+++ frontend/src/hooks/useUploadPresentation.tsx
@@ -1,41 +1,64 @@
 import { useEffect, useRef, useState } from "react";
-import { APIRequestStatus, APIRequestStatusEnum } from "../constants/api_request";
+import {
+  APIRequestStatus,
+  APIRequestStatusEnum,
+} from "../constants/api_request";
 import { presentationRepository } from "../contexts/presentation_info_context";
 import { PresentationToUploadEntity } from "../infrastructure/entities/presentation_entity";
 import { html_to_image } from "../utils/presentation/html_to_image";
+import UnauthorizedError from "../errors/UnautherizedError";
+import { useAuth } from "../contexts/auth_context";
+import { useNavigate } from "react-router-dom";
+import { ErrorType } from "../constants/errors";
 
 export const useUploadPresentation = () => {
-    const [status, setStatus] = useState<APIRequestStatus>({ status: APIRequestStatusEnum.NOT_STARTED });
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  const [status, setStatus] = useState<APIRequestStatus>({
+    status: APIRequestStatusEnum.NOT_STARTED,
+  });
 
-    const setLoading = async () => {
-        setStatus({ status: APIRequestStatusEnum.LOADING });
-    }
+  const setLoading = async () => {
+    setStatus({ status: APIRequestStatusEnum.LOADING });
+  };
 
-    const uploadPresentation = async (content: string, title?: string) => {
-        try {
-            const image = document.getElementById('slide-0');
-            if (image) {
-                const blobImage = await html_to_image(image);
-                await setLoading();
-                const presentation: PresentationToUploadEntity = {
-                    file: content,
-                    image: blobImage,
-                    title
-                }
-                const response = await presentationRepository.uploadPresentation(presentation);
-                if (response) {
-                    setStatus({ status: APIRequestStatusEnum.SUCCESS });
-                    return;
-                }
-                throw new Error('No se pudo subir la presentación');
-            }
-            throw new Error('No se pudo obtener la imagen de la presentación');
-        } catch (error: any) {
-            setStatus({ status: APIRequestStatusEnum.ERROR, message: error.message});
-        }
+  const uploadPresentation = async (content: string, title: string) => {
+    try {
+      const image = document.getElementById("slide-0");
+      if (!image) {
+        throw new Error(ErrorType.UPLOAD_ERROR);
+      }
+      if (!title || title === "") {
+        throw new Error(ErrorType.NO_TITLE);
+      }
+
+      const blobImage = await html_to_image(image);
+      await setLoading();
+      const presentation: PresentationToUploadEntity = {
+        file: content,
+        image: blobImage,
+        title,
+      };
+      const response = await presentationRepository.uploadPresentation(
+        presentation
+      );
+      if (response) {
+        setStatus({ status: APIRequestStatusEnum.SUCCESS });
+        return;
+      }
+      throw new Error(ErrorType.UPLOAD_ERROR);
+    } catch (error: any) {
+      const errorCode = error.response?.status;
+      console.log(errorCode);
+      if (errorCode === 401) {
+        logout();
+        navigate("/login");
+      }
+      setStatus({ status: APIRequestStatusEnum.ERROR, message: error.message });
     }
-    useEffect(() => {
-        console.log(status);
-    }, [status]);
-    return { status, uploadPresentation };
-}
+  };
+  useEffect(() => {
+    console.log(status);
+  }, [status]);
+  return { status, uploadPresentation };
+};
