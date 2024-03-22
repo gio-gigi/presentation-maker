ce3ae9376b109ad8bed6566399cce277c75511a1
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-21 20:38:21 -0600

    Se agregó el campo de creador al modelo de PresentationInfo

--- frontend/src/data/models/prod/presentation_info_model.ts
+++ frontend/src/data/models/prod/presentation_info_model.ts
@@ -1,7 +1,10 @@
+import { UserInfo } from "../../../infrastructure/entities/user";
+
 export interface PresentationInfoModel {
     idPresentation: number;
     title: string;
     content: string;
     imageURL: string;
     creationDate: Date;
+    creator: UserInfo;
 }
