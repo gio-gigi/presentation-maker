e2df77ae1ed691ecf467bec8a0f0b808d5ed88b4
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-22 13:24:44 -0600

    Se añadio el contexto a la aplicacion global

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
 import { AuthContextProvider } from './contexts/auth_context';
+import { MessageContextProvider } from './contexts/confirm_message_context';
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-        <RouterProvider router={router} />
+        <MessageContextProvider>
+          <RouterProvider router={router} />
+        </MessageContextProvider>
       </AuthContextProvider>
     </div>
   );
