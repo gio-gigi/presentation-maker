694d9374846a1c5027e8cebb37e103b329036d85
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-22 13:25:11 -0600

    Se agregaron los metodos para mostrar el mensaje

--- frontend/src/pages/presentation_maker/presentation_maker_page.tsx
+++ frontend/src/pages/presentation_maker/presentation_maker_page.tsx
@@ -9,17 +9,20 @@ import { HomePage } from '../home/home_page';
 import { InfoModal } from '../../components/info_modal/info_modal';
 import { useNavigate } from 'react-router-dom';
 import { BackButton } from '../../components/back_button/back_button';
+import { useConfirmMessage } from '../../contexts/confirm_message_context';
 
 export const PresentationMakerPage = () => {
     const {slides, onTextChange, content, onTitleChange, title} = usePresentationMaker();
     const {status, uploadPresentation} = useUploadPresentation();
     const navigate = useNavigate();
+    const { showMessage } = useConfirmMessage();
 
     const handleUpload = () => {
         uploadPresentation(content, title);
     }
 
     if (status.status === APIRequestStatusEnum.SUCCESS) {
+        showMessage('Presentacion subida con exito');
         navigate('/', {replace: true});
     }
 
@@ -34,7 +37,7 @@ export const PresentationMakerPage = () => {
         <div className='pm-page'>
             <BackButton />
             {
-                status.status === APIRequestStatusEnum.ERROR && <InfoModal message={status.message || 'Error inesperado'} />
+                status.status === APIRequestStatusEnum.ERROR && <InfoModal message={status.message || 'Error inesperado'} isAlert/>
             }
             <PresentationEditor onTextChange={onTextChange} onTitleChange={onTitleChange}/>
             <PresentationPreview slides={slides}/>
