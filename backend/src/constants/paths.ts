e1dbd16989313d4b7df0e0ffdd258bf5d6dd0193
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-04-08 11:05:47 -0600

    Se modifica la URL del backend donde se encuentran las imagenes

--- backend/src/constants/paths.ts
+++ backend/src/constants/paths.ts
@@ -1,5 +1,6 @@
 import { SERVER_PORT } from "./server";
+import { URL } from "./url";
 
 export const PATH_TO_STATICS_TXT = "static/presentations/txt/";
 export const PATH_TO_STATICS_IMG = "static/presentations/img/";
-export const BASE_IMG_URL = `http://localhost:${SERVER_PORT}/img/`;
+export const BASE_IMG_URL = `http://${URL}:${SERVER_PORT}/img/`;
