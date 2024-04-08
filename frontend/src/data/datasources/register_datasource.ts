03cd74af4ae8df3c17fb34d37fd574bdb64c8fa6
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-04-08 11:16:20 -0600

    Se reemplaza la URL de la API por la constante

--- frontend/src/data/datasources/register_datasource.ts
+++ frontend/src/data/datasources/register_datasource.ts
@@ -4,14 +4,13 @@ import axios from "axios";
 import { LoggedInUser } from "../../infrastructure/entities/user";
 import { LoggedInUserModel } from "../models/prod/LoggedInUserModel";
 import { UserRole } from "../../constants/roles";
-
-const API_URL = "http://localhost:3001";
+import { APIUrl } from "../../constants/api_url";
 
 export class RegisterDatasourceDev implements RegisterDatasource {
   async getToken(form: RegisterFormValues): Promise<LoggedInUser> {
     const { email, password, name } = form;
     const { status, data } = await axios.post<LoggedInUserModel>(
-      API_URL + "/api/auth/register",
+      APIUrl + "api/auth/register",
       {
         email: email,
         password: password,
